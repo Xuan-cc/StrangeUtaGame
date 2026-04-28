d6794c58d08da00c5dfac14078357a590d0daebb
Xuan-cc-msi <939918608@qq.com>
2026-04-28 21:06:46 +0800

    fix：更新版本号至0.2.2

--- src/strange_uta_game/frontend/settings/settings_interface.py
+++ src/strange_uta_game/frontend/settings/settings_interface.py
@@ -889,7 +889,7 @@ class SettingsInterface(ScrollArea):
         about_card = SettingCard(
             FIF.INFO,
             "StrangeUtaGame - 歌词打轴软件",
-            "版本 0.2.1 | 由 RhythmicaLyrics 启发",
+            "版本 0.2.2 | 由 RhythmicaLyrics 启发",
             self.about_group,
         )
         self.about_group.addSettingCard(about_card)
